b1897f437597afb68c0ae007f57ed27a757865de
Adil <78382272+AdilAsad1@users.noreply.github.com>
2023-01-19 21:28:10 -0500

    added user inputs

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 #get Input from user, keyword, consumerKey, consumerSecret, accessToken, accessSecret and bearerToken
 def getInput():
     keyword = input("Enter the keyword you want to search: ")
-    #consumer_key = input('Enter your Consumer Key: ')
-    #consumer_secret = input('Enter your Consumer Secret: ')
-    #access_token = input('Enter your Access Token: ')
-    #access_secret = input('Enter your Access Secret: ')
-    consumer_key = 'VfK92Z4a2wL3JMw3ueTpCk8tY'
-    consumer_secret = 'uF0EY7MVmliEKPOtalIqlN1PpvTKgxoMIV9XEaQk70CTanX8Sz'
-    access_token = '1354464657650708490-pTuMK61JAKi6Lja85bjXs7h3IwcM8w'
-    access_secret = 'ME5w2wB6DbbJ4YObLs9n3529yPjOMTwJEuBCKevzXe0fa'
+    consumer_key = input('Enter your Consumer Key: ')
+    consumer_secret = input('Enter your Consumer Secret: ')
+    access_token = input('Enter your Access Token: ')
+    access_secret = input('Enter your Access Secret: ')
+    #consumer_key = 'VfK92Z4a2wL3JMw3ueTpCk8tY'
+    #consumer_secret = 'uF0EY7MVmliEKPOtalIqlN1PpvTKgxoMIV9XEaQk70CTanX8Sz'
+    #access_token = '1354464657650708490-pTuMK61JAKi6Lja85bjXs7h3IwcM8w'
+    #access_secret = 'ME5w2wB6DbbJ4YObLs9n3529yPjOMTwJEuBCKevzXe0fa'
     return keyword, consumer_key, consumer_secret, access_token, access_secret
 
 def stopWordLst():
@@ -32,7 +32,7 @@ def loadApi(consumer_key, consumer_secret, access_token, access_secret):
     return tweepy.API(auth)
 
 def getTweets(api,keyword):
-    tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en",tweet_mode='extended').items(10)
+    tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en",tweet_mode='extended').items(1000)
     list_tweets = [tweet for tweet in tweets]
     tweet_data = []
     for tweet in list_tweets:
